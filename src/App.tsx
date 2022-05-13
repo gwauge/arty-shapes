5a81480616453a2586da956d6c3a5b7bc2473a62
Georg <georg.auge01@gmail.com>
2022-05-13 13:49:02 +0200

    Feat: basic prototyping layout
    - added bootstrap for easier UI design
    - added sample input images to start prototyping

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -9,6 +9,27 @@ function App() {
         <h1>Arty Shapes</h1>
         <h3>by Anton & Georg</h3>
       </header>
+
+      <main>
+        <div className='container-lg' style={{minHeight:"100vh"}}>
+          <div className='row align-items-strech'>
+
+			<div className='col-12 col-lg-6 row'>
+				<div className='col-12'>
+          			<img src="/input1.jpg" alt="Source" className='border border-primary' height={250}/>
+				</div>
+				<div className='col-12'>
+          			<img src="/segmentation1.png" alt="Segmentation" className='border border-primary' height={250}/>
+				</div>
+			</div>
+
+			<div className='col-12 col-lg-6'>
+          		<canvas id="as-canvas" style={{width: "100%"}} className='border border-primary m-auto'/>
+			</div>
+
+		  </div>
+        </div>
+      </main>
     </div>
   );
 }
