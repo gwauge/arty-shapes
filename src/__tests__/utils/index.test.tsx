0ce3861ae3e1ffb65c2809649a25c69f37183ca7
Georg Auge <georg.auge01@gmail.com>
2022-05-22 01:17:27 +0200

    Feat: downsize image to increase performance
    - using nearest-neighbor interpolation
    - dramatically increases compute time
    - segmentation images don't need high resolution

--- src/__tests__/utils/index.test.tsx
+++ src/__tests__/utils/index.test.tsx
@@ -1,4 +1,4 @@
-import { hexToRgb, rgbToHex, componentToHex } from '../../utils';
+import { hexToRgb, rgbToHex, componentToHex, i_to_xy, xy_to_i } from '../../utils';
 
 test.each([
     ["#000000", { r: 0, g: 0, b: 0 }],
@@ -6,7 +6,7 @@ test.each([
     ["bd5c55", { r: 189, g: 92, b: 85 }],
     ["#buic55", null],
 ])("should convert HEX %s to RGB %s", (hex, rgb) => {
-    expect(hexToRgb(hex)).toEqual(rgb)
+    expect(hexToRgb(hex)).toEqual(rgb);
 })
 
 test.each([
@@ -14,7 +14,7 @@ test.each([
     [{ r: 255, g: 255, b: 255 }, "#ffffff"],
     [{ r: 189, g: 92, b: 85 }, "#bd5c55"]
 ])("should convert RGB %s to HEX %s", (rgb, hex) => {
-    expect(rgbToHex(rgb.r, rgb.g, rgb.b)).toEqual(hex)
+    expect(rgbToHex(rgb.r, rgb.g, rgb.b)).toEqual(hex);
 })
 
 test.each([
@@ -22,5 +22,22 @@ test.each([
     [255, "ff"],
     [189, "bd"]
 ])("should convert base10 integer %i to base16 %s", (rgb, hex) => {
-    expect(componentToHex(rgb)).toEqual(hex)
+    expect(componentToHex(rgb)).toEqual(hex);
+})
+
+test.each([
+    [0, [0, 0]],
+    [12, [3, 0]],
+    [84, [1, 2]],
+    [86, [1, 2]],
+])("should convert index %i to coordinates %s", (i, xy) => {
+    expect(i_to_xy(i, 10)).toEqual(xy);
+})
+
+test.each([
+    [[0, 0] as [number, number], 0],
+    [[3, 0] as [number, number], 12],
+    [[1, 2] as [number, number], 84],
+])("should convert index %i to coordinates %s", (xy, i) => {
+    expect(xy_to_i(xy, 10)).toEqual(i);
 })
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,10 +10,81 @@ export function hexToRgb(hex: string) {
 export function componentToHex(c: number) {
     const hex = c.toString(16);
     return hex.length === 1 ? "0" + hex : hex;
-  }
-  
+}
+
 export function rgbToHex(r: number, g: number, b: number) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+/*
+early version of the segment data structure
+type Segment = {
+    id: number,
+    aabb: {
+        'n': number
+        's': number
+        'e': number
+        'w': number
+    },
+    segmentation_color: string,
+
+}
+*/
+
+/**
+ * Resize an image using nearest-neighbor sampling.
+ * @param img Image to resize
+ * @param width Target  width
+ * @param height Target height
+ * @returns Resized image
+ */
+export function nearest_neighbor(img: ImageData, width: number, height: number) {
+    const new_img = new ImageData(width, height);
+    const scale_x = img.width / width;
+    const scale_y = img.height / height;
+
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            const px = Math.floor(j * scale_x);
+            const py = Math.floor(i * scale_y);
+
+            const index_new = xy_to_i([j, i], width);
+            const index_old = xy_to_i([px, py], img.width);
+            new_img.data[index_new + 0] = img.data[index_old + 0];
+            new_img.data[index_new + 1] = img.data[index_old + 1];
+            new_img.data[index_new + 2] = img.data[index_old + 2];
+            new_img.data[index_new + 3] = img.data[index_old + 3];
+        }
+    }
+    return new_img;
+}
+
+/**
+ * Converts a linear array position to a 2D position. 
+ * Note that `ImageData` arrays are exprected, i.e. rgba 
+ * each have their own index but result in the same pixel 
+ * coordinates.
+ * @param i Position in the array
+ * @param width Width of the image
+ * @returns Array of x and y coordinate in the image space
+ */
+export function i_to_xy(i: number, width: number) {
+    return [
+        Math.floor((i / 4) % width),
+        Math.floor((i / 4) / width)
+    ]
+}
+
+/**
+ * Converts a 2D position to a linear array position. 
+ * Note that `ImageData` arrays are presumed, i.e. rgba 
+ * each pixel coordinate has 4 array positions.
+ * @param i Position in the array
+ * @param width Width of the image
+ * @returns Array of x and y coordinate in the image space
+ */
+export function xy_to_i([x, y]: [number, number], width: number) {
+    return y * width * 4 + x * 4;
 }
 
 export function shapify(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
@@ -24,11 +95,15 @@ export function shapify(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
     const segmentation_image = document.getElementById('img-segmentation') as HTMLImageElement;
     canvas.height = segmentation_image.naturalHeight;
     canvas.width = segmentation_image.naturalWidth;
-    
+
     ctx.drawImage(segmentation_image, 0, 0);
 
-    const image_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    if (!image_data) return;
+    const original_img = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    if (!original_img) return;
+    // const image_data = original_img;
+
+    // downscale image using nearest neighbor sampling
+    const image_data = nearest_neighbor(original_img, segmentation_image.width, segmentation_image.height);
 
     // data structure for storing the bounding box for each color
     const aabb = new Map<string, {
@@ -45,8 +120,7 @@ export function shapify(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
         const b = image_data.data[i + 2];
         // const a = image_data.data[i + 3];
 
-        const x = (i / 4) % canvas.width;
-        const y = Math.floor((i / 4) / canvas.width);
+        const [x, y] = i_to_xy(i, image_data.width);
 
         const hex = rgbToHex(r, g, b);
         const old_bb = aabb.get(hex);
@@ -67,12 +141,14 @@ export function shapify(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
         }
     }
 
+    console.log("aabb", aabb);
+
     // clear the canvas for redrawing
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
     // ensure canvas has the same size as the rendered image
     const target_size = segmentation_image.height;
-    const scaling_factor = target_size / canvas.height;
+    const scaling_factor = target_size / image_data.height;
     canvas.height = segmentation_image.height;
     canvas.width = segmentation_image.width;
     ctx.scale(scaling_factor, scaling_factor);
@@ -88,5 +164,5 @@ export function shapify(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
 
         ctx.fillRect(bb.w, bb.n, bb.e - bb.w, bb.s - bb.n);
     })
-    
+
 }
